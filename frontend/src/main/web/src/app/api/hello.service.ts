1bcf8aede16ce13fdb6af58f20df0cdbf6039f4e
piotrkosinski1992@o2.pl <piotrkosinski1992@o2.pl>
2020-03-29 20:10:29 +0200

    hopefully last path configuration commit

--- frontend/src/main/web/src/app/api/hello.service.ts
+++ frontend/src/main/web/src/app/api/hello.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
+import {environment} from "../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +11,6 @@ export class HelloService {
   constructor(private http: HttpClient) { }
 
   getGreeting(): Observable<any> {
-    return this.http.get('/api/hello');
+    return this.http.get(environment.PROXY_PATH + 'hello');
   }
 }
