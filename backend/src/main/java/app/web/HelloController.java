f62f1d6b7cb5a3e962aed3dad1495549f30d3173
piotrkosinski1992@o2.pl <piotrkosinski1992@o2.pl>
2020-03-30 22:18:39 +0200

    removed non existing service

--- backend/src/main/java/app/web/HelloController.java
+++ backend/src/main/java/app/web/HelloController.java
@@ -1,17 +1,10 @@
 package app.web;
 
-import app.service.GameService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-
-  private final GameService gameService;
-
-  public HelloController(GameService gameService) {
-    this.gameService = gameService;
-  }
 
   @GetMapping("/hello")
   public Message sayHello() {
